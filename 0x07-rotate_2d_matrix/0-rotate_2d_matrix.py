6b9744cbfd7e90bfd66368d3031d86293899d019
yanbel0706 <yanis.belk.edu@gmail.com>
2024-09-17 13:11:23 +0100

    task resolved

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+this module contains the rotate_2d_matrix method
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    '''
+    the rotate_2d_matrix method
+    '''
+    tempMat = []
+    n = len(matrix)
+    for i in range(n):
+        tempMat.append([0] * n)
+    for i in range(n):
+        for j in range(n):
+            tempMat[j][n - 1 - i] = matrix[i][j]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = tempMat[i][j]
